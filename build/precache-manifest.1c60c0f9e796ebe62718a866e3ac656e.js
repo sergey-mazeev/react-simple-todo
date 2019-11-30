9b9643eb3d33b6097d052009dbd385fe25dc23d0
Sergey Mazeev <sergey.mazeev@gmail.com>
2019-11-30 23:30:36 +0500

    Build folder for github pages

---
+++ build/precache-manifest.1c60c0f9e796ebe62718a866e3ac656e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887d45cb6ccfb5fe3cf2852b0109a132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cf50d5ee25c290a3a02",
+    "url": "/static/css/3.a4f5cac6.chunk.css"
+  },
+  {
+    "revision": "a073d65d24c4ad766ba6",
+    "url": "/static/css/main.e0e344f3.chunk.css"
+  },
+  {
+    "revision": "5d5b29bd62c51d958d68",
+    "url": "/static/js/2.17c9dba6.chunk.js"
+  },
+  {
+    "revision": "3cf50d5ee25c290a3a02",
+    "url": "/static/js/3.13b4f6fc.chunk.js"
+  },
+  {
+    "revision": "a073d65d24c4ad766ba6",
+    "url": "/static/js/main.512b184f.chunk.js"
+  },
+  {
+    "revision": "6dcec96b5a9d2aeadeea",
+    "url": "/static/js/runtime-main.03194a21.js"
+  }
+]);
